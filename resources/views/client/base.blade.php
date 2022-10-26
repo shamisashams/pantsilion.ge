09dbfb181fc58f655085766713e9cd8184e56968
Alex Zhukov <alex3dfree@gmail.com>
2022-10-26 15:12:56 +0400

    update fix

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -99,13 +99,14 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '116974165309440');
+        fbq('init', '488788129945036');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=116974165309440&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=488788129945036&ev=PageView&noscript=1"
         /></noscript>
-    <!-- End Meta Pixel Code -->
+    <!-- End Meta Pixel Code -->
+
 </head>
 <body>
 <!-- Messenger Chat Plugin Code -->
