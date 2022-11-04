213622453a0c7519976835a37730e1b6357eab49
Alex Zhukov <alex3dfree@gmail.com>
2022-11-04 17:08:23 +0400

    update fix

--- app/Http/Controllers/Client/FavoriteController.php
+++ app/Http/Controllers/Client/FavoriteController.php
@@ -290,6 +290,7 @@ class FavoriteController extends Controller
 
         if($product->promocode){
             if($product->promocode->status){
+                dd($product->promocode);
                 $promo_gen = new Promocode();
                 if($request->user()->promocode()->where('promocode_id',$product->promocode->id)->count() === 0){
                     $gen = $promo_gen->generateCode();
