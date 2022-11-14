70b5cae87528017e797b0bfe4d64acc345f5ac71
shamisashams <shamisaashams@gmail.com>
2022-11-14 17:08:34 +0400

    hero section bug fixed

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -48,14 +48,14 @@ const Home = ({ seo }) => {
         <Layout seo={seo}>
             <div className="overflow-hidden">
                 {collection ? (
-                    <section className="lg:h-screen bg-gray-50 relative">
-                        <div className="wrapper lg:h-full lg:pt-0 pt-40 flex items-center justify-between ">
-                            <div className="lg:w-3/5 lg:mr-5">
+                    <section className="lg:min-h-screen bg-gray-50 relative ">
+                        <div className="wrapper lg:min-h-screen lg:pt-0 pt-40 flex items-center justify-between ">
+                            <div className="lg:w-3/5 lg:mr-5 lg:pt-36">
                                 <div className="lg:text-6xl text-4xl max-w-3xl bold ">
                                     {/* Super quality furniture for your home */}
                                     {collection.title}
                                 </div>
-                                <p className="lg:my-10 my-6 max-w-3xl text-justify">
+                                <p className="lg:my-10 my-6 lg:mr-5 max-w-3xl text-justify">
                                     {/* Choose from a wide range of premium quality wooden furniture
                                 online. Comfort is our priority to satisfy our customers, and we
                                 provide all the furniture that you can easily and quickly get in
